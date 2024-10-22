0759ad391623a725e9047f11d92e09b89f3aaf5b
bellayun <147754003+bellayun@users.noreply.github.com>
2024-10-22 15:19:57 -0400

    changes

---
+++ coffee machine project.py
@@ -0,0 +1,25 @@
+
+
+# Program requirement
+# 1. print report
+# 2. check resources are sufficient
+# 3. process coins
+# 4. check transaction successful?
+# 5. make coffee (deduct resources)
+
+# What would you like? (espresso/latte/cappuccino): report
+# Water: 300ml
+# Milk: 200ml
+# Coffee: 100g
+# Money: $0
+# What would you like? (espresso/latte/cappuccino): latte
+# Please insert coins.
+# How many quarters?: 
+# How many dimes?:
+# How many nickles?:
+# How many pennies?:
+# Here is $2.42 in change
+# Here is your latte. Enjoy!
+# What would you like?
+
+#TODO: 1. Print report of all coffee machine resources
